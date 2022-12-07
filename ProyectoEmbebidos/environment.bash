150111a7ed8bbc80021b7fb7332dfb1308e64b91
Julian <jdescobarj@unal.edu.co>
2022-12-07 08:07:21 -0500

    Espero que funcione

---
+++ ProyectoEmbebidos/environment.bash
@@ -0,0 +1,5 @@
+export RIOTBASE=$HOME/riot-sdk
+# export PATH=$PATH:$HOME/.local/bin/
+export PATH=$PATH:$HOME/.espressif/tools/xtensa-esp32-elf/esp-2021r2-patch3-8.4.0/xtensa-esp32-elf/bin/
+
+# . $RIOTBASE/dist/tools/esptools/export.sh esp32 >> log
